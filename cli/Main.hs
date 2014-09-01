7f12e01919743f427ae58b4270a9a8e5aa6cd165
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2014-09-01 14:50:53 +0200

    Add CLI option parsing
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- cli/Main.hs
+++ cli/Main.hs
@@ -3,12 +3,15 @@ module Main (main)
 
 import Prelude hiding (writeFile)
 
+import Control.Monad (when)
 import Data.String (fromString)
 import Data.Text.IO (writeFile)
-import System.Environment (getArgs)
 import System.FilePath ( (<.>)
-                       , dropExtension
+                       , (</>)
+                       , takeBaseName
                        )
+
+import Options.Applicative
 
 import Milkman.IO ( parseFile
                   , showBurmeister
@@ -17,59 +20,100 @@ import Milkman.Context ( Concept
                        , Context
                        , concepts
                        , showIncidence
-                       , tightCrosses
                        )
 import Milkman.Covers ( conceptualCovers
                       , minimalCovers
                       )
 import Milkman.Factors (factorContexts)
 
-writeFactors :: String -> Context -> (Int, [Concept]) -> IO ()
-writeFactors base cxt (idx, cover) = do
-  let base' = concat [ dropExtension base
+data Options = Options { outputPrefix :: String
+                       , verbose :: Bool
+                       , preconceptual :: Bool
+                       , inputFiles :: [String]
+                       }
+
+put :: Context -> IO ()
+put c = putStrLn (showIncidence c) >> putStrLn ""
+
+writeFactors :: Options -> String -> Context -> (Int, [Concept]) -> IO ()
+writeFactors opts input cxt (idx, cover) = do
+  let out n = concat [ outputPrefix opts </> takeBaseName input
                      , "-factorization-"
                      , show idx
-                     ]
+                     , "-"
+                     , n
+                     ] <.> "cxt"
   (gf, fm) <- factorContexts cxt cover
 
-  putStrLn $ showIncidence gf
-  putStrLn ""
+  when (verbose opts) $ do
+    putStrLn $ "Factorization " <> (show idx) <> ":"
+    put gf
+    put fm
 
-  putStrLn $ showIncidence fm
-  putStrLn ""
+  putStrLn $ "Writing `" <> (out "objects") <> "'."
+  writeFile (out "objects") $ showBurmeister gf
+  putStrLn $ "Writing `" <> (out "attributes") <> "'."
+  writeFile (out "attributes") $ showBurmeister fm
 
-  writeFile (base' ++ "-objects" <.> "cxt") $ showBurmeister gf
-  writeFile (base' ++ "-attributes" <.> "cxt") $ showBurmeister fm
+  when (preconceptual opts) $ do
+    let (mo, ma) = minimalCovers cover
+    print . length $ mo
+    putStrLn ""
+    mapM_ (\s -> print s >> putStrLn "") mo
 
-  let (mo, ma) = minimalCovers cover
-  print . length $ mo
-  putStrLn ""
-  mapM_ (\s -> print s >> putStrLn "") mo
+    print . length $ ma
+    putStrLn ""
+    mapM_ (\s -> print s >> putStrLn "") ma
 
-  print . length $ ma
-  putStrLn ""
-  mapM_ (\s -> print s >> putStrLn "") ma
+factor :: Options -> IO ()
+factor opts = do
+  when (verbose opts) $ do
+    putStrLn $ "Outputting to " <> outputPrefix opts
+    putStrLn ("Computing "
+              <> (if preconceptual opts then "pre" else "")
+              <> "conceptual covers.")
+  mapM_ (factor' opts) $ inputFiles opts
 
-factor :: String -> IO ()
-factor s = do
+factor' :: Options -> String -> IO ()
+factor' opts s = do
+  putStrLn $ "Processing `" <> s <> "'."
   eec <- parseFile $ fromString s
   case eec of
-    Left err -> print $ "failed reading context: " ++ err
+    Left err -> print $ "failed reading context: " ++ err -- FIXME STDERR
     Right cxt -> do
-      put cxt
+      when (verbose opts) $ put cxt
 
       let cs = conceptualCovers cxt
 
-      print . length $ concepts cxt
-      print . length $ cs
+      putStrLn ("Context has "
+                <> (show . length . concepts $ cxt)
+                <> " concepts.")
+      putStrLn ("Context has "
+                <> (show . length $ cs)
+                <> " minimal conceptual covers.")
       putStrLn ""
 
-      mapM_ (writeFactors s cxt) $ zip [1..] (take 1 cs)
+      mapM_ (writeFactors opts s cxt) $ zip [1..] cs
 
-      let tis = tightCrosses cxt
-      put tis
-  where put c = putStrLn (showIncidence c) >> putStrLn ""
+options :: Parser Options
+options = Options
+          <$> strOption (long "output-prefix"
+                         <> short 'o'
+                         <> metavar "PREFIX"
+                         <> help "Prefix for the output files")
+          <*> switch (long "verbose"
+                      <> short 'v'
+                      <> help "Whether to additional information")
+          <*> switch (long "minimal-covers"
+                      <> help ("Whether to to generate minimal "
+                      ++ "pre-conceptual covers instead of conceptual covers"))
+          <*> many (argument str $ metavar "CONTEXTS...")
 
 
 main :: IO ()
-main = getArgs >>= mapM_ factor
+main = execParser opts >>= factor
+  where opts = info (helper <*> options)
+               (fullDesc
+                <> progDesc ("Compute (pre-)conceptual covers of "
+                             ++ "boolean contexts")
+                <> header "milkman - boolean context covering")
